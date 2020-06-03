dd207d281e57abbb2f27792a9c7d11596399fe08
lomogi <fillitpost@gmail.com>
2020-06-03 16:52:26 +0900

    Update submit.php

--- submit.php
+++ submit.php
@@ -6,7 +6,7 @@
 	<body>
 		<h1>전달받은 데이터</h1>
 		<ul>
-			<li>당신이 검색한 단어는 <?php echo $_GET["keyword"]; ?>입니다.</li>
+			<li>당신이 검색한 단어는 <?php echo $_GET['keyword']; ?>입니다.</li>
 		</ul>
 	</body>
 </html>
